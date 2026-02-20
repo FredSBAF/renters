55ebe9355604e59000d03b29f2693521160641a8
Fred <fdetez@sbafholding.com>
2026-02-20 14:40:12 +0100

    refactor(server): replace console log with Winston logger for server startup message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import { config } from './config/env';
 import app from './app';
+import { logger } from './utils/logger';
 
 app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+  logger.info(`Server running on port ${config.port}`);
 });
